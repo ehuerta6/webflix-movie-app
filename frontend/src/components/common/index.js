a2bd46e2315433d64d147ff2ea676e991bee919f
Emiliano <ehuerta6@miners.utep.edu>
2025-05-02 19:34:46 -0600

    Updates

--- frontend/src/components/common/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button'
-export { default as Card } from './Card'
-export { default as IconButton } from './IconButton'
-export { default as Spinner } from './Spinner'
-export { default as LoadingState } from './LoadingState'
--- frontend/src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useDebounce } from './useDebounce'
-export { default as useOutsideClick } from './useOutsideClick'
-export { default as useLoadingError } from './useLoadingError'
